2450df9d5927112889488b82684b698f3cd9beba
Sanela Dekanić <sdekanic@pmfst.hr>
2020-10-25 12:03:06 +0100

    Dodatno

---
+++ build/precache-manifest.8500f5462de7a8938845f4820b79aecc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88ee3750d8ece946f7bc7531804157f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f89864d02d12c662aac",
+    "url": "/static/css/main.90ffd6ca.chunk.css"
+  },
+  {
+    "revision": "75e5ba4b459a4b80655a",
+    "url": "/static/js/2.7d5328f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d5328f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f89864d02d12c662aac",
+    "url": "/static/js/main.df267c17.chunk.js"
+  },
+  {
+    "revision": "82664534ad3b166a4ff3",
+    "url": "/static/js/runtime-main.5763738a.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,105 @@
+const express = require('express')
+const app = express()
+
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+
+
+const zahtjevInfo = (req, res, next) => {
+    console.log('Metoda:', req.method)
+    console.log('Putanja:', req.path)
+    console.log('Tijelo:', req.body)
+    console.log('---')
+    next()
+  }
+  
+  app.use(zahtjevInfo)
+  
+
+
+let poruke = [
+    {
+        id: 4,
+        sadrzaj: 'HTML nije jednostavan',
+        vazno: true
+    },
+    {
+        id: 8,
+        sadrzaj: 'React koristi JSX sintaksu',
+        vazno: false
+    },
+    {
+        id: 13,
+        sadrzaj: 'GET i POST su najvaznije metode HTTP protokola',
+        vazno: true
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Pozdrav od Express servera + nodemona</h1>')
+})
+
+app.get('/api/poruke', (req, res) => {
+    res.json(poruke)
+})
+
+app.get('/api/poruke/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const poruka = poruke.find(p => p.id === id)
+
+    if (poruka) {
+        res.json(poruka)
+    } else {
+        res.status(404).end()
+    }
+
+})
+app.delete('/api/poruke/:id', (req, res) => {
+    const id = Number(req.params.id)
+    poruke = poruke.filter(p => p.id !== id)
+    res.status(204).end()
+
+})
+
+app.put('/api/poruke/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const podatak = req.body
+    poruke = poruke.map(p => p.id !== id ? p : podatak)
+    res.json(podatak)
+
+})
+
+app.post('/api/poruke', (req, res) => {
+    const maxId = poruke.length > 0
+    ? Math.max(...poruke.map(p => p.id))
+    : 0
+
+    const podatak = req.body
+    if(!podatak.sadrzaj){
+        return res.status(400).json({
+            error: 'Nedostaje sadržaj poruke'
+        })
+    }
+    const poruka = {
+        sadrzaj: podatak.sadrzaj,
+        vazno: podatak.vazno || false,
+        datum: new Date(),
+        id: maxId + 1
+    }
+
+    poruke = poruke.concat(poruka) 
+    res.json(poruka)
+})
+
+const nepoznataRuta = (req, res) => {
+    res.status(404).send({ error: 'nepostojeca ruta' })
+  }
+  
+  app.use(nepoznataRuta)
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server sluša na portu ${PORT}`);
+})
